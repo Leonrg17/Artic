e96f55b5f9a67eb80104b22803ba164b261e32ab
Jellyfish25 <mitkodimitrov50@gmail.com>
2022-08-04 20:21:48 -0700

    added constant file

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// potentially make this the entire button, constant button for confirm?
+const kSendButtonTextStyle = TextStyle(
+  color: Colors.lightBlueAccent,
+  fontWeight: FontWeight.bold,
+  fontSize: 18.0,
+);
